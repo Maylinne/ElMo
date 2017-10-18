75e60c508279758fffe0b448c99b6edb3035c064
Maylinne <nagy.anasztazia@gmail.com>
2017-10-18 16:41:26 +0200

    Food counter

--- app/src/main/java/com/example/android/elmo/FeedingFragment.java
+++ app/src/main/java/com/example/android/elmo/FeedingFragment.java
@@ -30,6 +30,13 @@ public class FeedingFragment extends Fragment implements IRefreshView {
     TextView hungerText = null;
     static int foodElement = 0;
     RadioGroup rg = null;
+    ToggleButton rb = null;
+
+    ToggleButton airTb = null;
+    ToggleButton earthTb = null;
+    ToggleButton fireTb = null;
+    ToggleButton waterTb = null;
+    ArrayList<Food> foodList = StableSingleton.getInstance().getFoodArray();
 
 
     @Override
@@ -43,6 +50,19 @@ public class FeedingFragment extends Fragment implements IRefreshView {
         // Toggle button instantiate
         rg = ((RadioGroup) feedingView.findViewById(R.id.toggleGroup));
         rg.setOnCheckedChangeListener(ToggleListener);
+
+        airTb = (ToggleButton) feedingView.findViewById(R.id.FOOD_CLOUD);
+        earthTb = (ToggleButton) feedingView.findViewById(R.id.FOOD_DIRT);
+        fireTb = (ToggleButton) feedingView.findViewById(R.id.FOOD_COAL);
+        waterTb = (ToggleButton) feedingView.findViewById(R.id.FOOD_WATER);
+
+        SetAmountText(airTb);
+        SetAmountText(earthTb);
+        SetAmountText(fireTb);
+        SetAmountText(waterTb);
+
+
+
 
         BindToggleButtonsClick(rg);
 
@@ -85,6 +105,7 @@ public class FeedingFragment extends Fragment implements IRefreshView {
                         act.toast = Toast.makeText(feedingView.getContext(), speach.get(pos), Toast.LENGTH_SHORT);
                         act.monc.mHunger += r.nextInt(30 - 20) + 20;
                         StableSingleton.getInstance().DecreaseFoodAmount(foodElement, 1);
+                        SetAllAmountTexts(rg);
                         if (act.monc.mHunger >= 100) {
                             act.monc.Heal();
                             act.monc.mHunger = 0;
@@ -113,12 +134,13 @@ public class FeedingFragment extends Fragment implements IRefreshView {
 
     private void BindToggleButtonsClick(RadioGroup radioGroup) {
         for (int i = 0; i < radioGroup.getChildCount(); i++) {
-            ToggleButton rb = (ToggleButton) radioGroup.getChildAt(i);
+            rb = (ToggleButton) radioGroup.getChildAt(i);
             rb.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     ((RadioGroup)view.getParent()).check(view.getId());
                     foodElement = Integer.parseInt(view.getTag().toString());
+                    SetAmountText(rb);
                 }
             });
         }
@@ -150,6 +172,30 @@ public class FeedingFragment extends Fragment implements IRefreshView {
     public void SetAttributes () {
         hungerMeter.setProgress(act.monc.mHunger);
         hungerText.setText(act.monc.HungerTextSelect());
+        SetAllAmountTexts(rg);
+    }
+
+
+    private void SetAllAmountTexts (RadioGroup radioGroup) {
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            rb = (ToggleButton) radioGroup.getChildAt(i);
+            SetAmountText(rb);
+
+        }
+    }
+
+    private void SetAmountText(ToggleButton rb) {
+
+        int size = foodList.size();
+        for (int i = 0; i < size; i++)
+        {
+            if (Integer.parseInt(rb.getTag().toString()) == foodList.get(i).getElement()) {
+                rb.setTextOn(String.valueOf(foodList.get(i).getAmount()));
+                rb.setTextOff(String.valueOf(foodList.get(i).getAmount()));
+                rb.setText(String.valueOf(foodList.get(i).getAmount()));
+                return;
+            }
+        }
     }
 
 
